81d94abd2be6296a4a1adc4081088941e462f158
eunseonglee <eunseongofficial@gmail.com>
2021-11-15 22:52:49 +0900

    보일러 플레이트 적용

--- crewspace-mobile/src/environments.ts
+++
@@ -1,9 +0,0 @@
-import { DEV_BASE_URL, PROD_BASE_URL, KAKAO_AUTH_REST_API_ID } from '@env';
-
-const ENV = {
-  apiUrl: __DEV__ ? DEV_BASE_URL : PROD_BASE_URL,
-  kakaoClientId: KAKAO_AUTH_REST_API_ID,
-  redirectUrl: `${__DEV__ ? DEV_BASE_URL : PROD_BASE_URL}/auth`,
-};
-
-export default ENV;
---
+++ mobile/.prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  jsxSingleQuote: true,
+};
