9f19b6430bd5d9d856d943c9410c6376875648b9
eunseonglee <eunseongofficial@gmail.com>
2021-11-16 18:07:06 +0900

    프로젝트 alias 설정

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'module-resolver',
+    {
+      root: ['.'],
+      extensions: [
+        '.ios.ts',
+        '.android.ts',
+        '.ts',
+        '.ios.tsx',
+        '.android.tsx',
+        '.tsx',
+        '.jsx',
+        '.js',
+        '.json',
+      ],
+      alias: {
+        '~': './src',
+        'assets/*': './srcassets/*',
+        'components/*': './srccomponents/*',
+        'hooks/*': './srchooks/*',
+        'mocks/*': './srcmocks/*',
+        'navigation/*': './srcnavigation/*',
+        'screens/*': './srcscreens/*',
+        'store/*': './srcstore/*',
+        'styles/*': './srcstyles/*',
+        'theme/*': './srctypes/*',
+        'types/*': './srctypes/*',
+        'utils/*': './srcutils/*',
+      },
+    },
+  ],
 };
