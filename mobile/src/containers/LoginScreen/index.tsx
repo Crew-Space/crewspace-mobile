b6733e48c7ab6283902957936ab15eca2fbdb2c7
eunseonglee <eunseongofficial@gmail.com>
2021-11-17 02:14:58 +0900

    [#1] feat: 로그인 화면 마크업

--- mobile/src/containers/LoginScreen/index.tsx
+++ mobile/src/containers/LoginScreen/index.tsx
@@ -1,77 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useCallback } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { SvgXml } from 'react-native-svg';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/core';
+
+import { logo } from 'assets/svg';
+import kakaoLogin from 'assets/svg/kakaoLogin';
+import { BLACK, GRAY2, WHITE } from 'theme/Colors';
 import Text from 'components/Text';
+import { RootRouterParams } from 'types/Route';
 
 const LoginScreen = () => {
+  const navigation = useNavigation<RootRouterParams>();
+
+  const onPress = useCallback(() => {
+    const spaceList = [];
+    navigation.replace(spaceList.length ? 'Main' : 'Invitation');
+  }, [navigation]);
+
   return (
-    <View>
-      <ScrollView>
-        <Text fontType={{ size: 20, weight: 'B' }}>초대코드를 입력하여 동아리에 참여해 주세요</Text>
-        <Text>초대코드를 입력하여 동아리에 참여해 주세요</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text>LoginScreen</Text>
-        <Text style={{ color: 'black' }}>LoginScreen</Text>
-      </ScrollView>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{ marginTop: '15%' }}>
+        <Text fontType={'REGULAR_16'} style={styles.subTitle}>
+          우리 동아리만의{'\n'}모바일 공간
+        </Text>
+        <SvgXml xml={logo} />
+      </View>
+      <View style={{ marginBottom: '12%' }}>
+        <SvgXml xml={kakaoLogin} onPress={onPress} />
+        <Text paragraph fontType={'REGULAR_14'} style={styles.policyText}>
+          계정 생성 시 크루 스페이스의{'\n'}
+          개인정보 수집 방침 및 이용약관에 동의하게 됩니다.
+        </Text>
+      </View>
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flex: 1,
+    backgroundColor: BLACK,
+    padding: 32,
+  },
+  subTitle: { color: WHITE, lineHeight: 30, marginBottom: 14 },
+  policyText: { color: GRAY2, textAlign: 'center', marginTop: 20 },
+});
 
 export default LoginScreen;
