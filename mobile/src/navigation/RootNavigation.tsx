a7bca19b4e943317b9f78324b79378eac31f1cbc
eunseonglee <eunseongofficial@gmail.com>
2021-11-17 18:49:26 +0900

    feat: 초대코드, 웬컴스크린 라우트 설정

--- mobile/src/navigation/RootNavigation.tsx
+++ mobile/src/navigation/RootNavigation.tsx
@@ -19,7 +19,13 @@ const RootNavigation = () => {
           headerShown: false,
         }}
       />
-      <Stack.Screen name='Invitation' component={InvitationNavigation} />
+      <Stack.Screen
+        name='Invitation'
+        component={InvitationNavigation}
+        options={{
+          headerShown: false,
+        }}
+      />
       <Stack.Screen name='Main' component={MainNavigation} />
       <Stack.Screen name='Post' component={LoginScreen} />
       <Stack.Screen name='EditCategory' component={LoginScreen} />
--- mobile/src/types/Route.ts
+++ mobile/src/types/Route.ts
@@ -1,10 +1,13 @@
 import { StackNavigationProp } from '@react-navigation/stack';
 import { BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import { RouteProp } from '@react-navigation/native';
 
 export type RootRouterParamList = {
   Auth: undefined;
   Contents: undefined;
-  Invitation: undefined;
+  Invitation: {
+    screen: keyof InvitationParamList;
+  };
   Main: undefined;
   Post: undefined;
   EditCategory: undefined;
@@ -16,7 +19,25 @@ export type InvitationParamList = {
   SpaceCode: undefined;
   CreateSpace: undefined;
   EnterSpace: undefined;
+  Welcome: {
+    darkTheme?: boolean;
+    data: ScreenProps;
+  };
 };
+
+type ProfileType = {
+  name: string;
+  imageUrl: string;
+  description: string;
+};
+
+interface ScreenProps {
+  title?: string;
+  profile: ProfileType;
+  mainButtonName: string;
+  linkButtonName?: string;
+  spaceInvitationCode?: string;
+}
 
 export type MainRouterParamList = {
   Home: undefined;
@@ -36,7 +57,11 @@ export type SearchParamList = {
 };
 
 export type RootRouterParams = StackNavigationProp<RootRouterParamList>;
-export type InvitationParams = BottomTabNavigationProp<InvitationParamList>;
+export type InvitationParams = StackNavigationProp<InvitationParamList>;
 export type MainRouterParams = BottomTabNavigationProp<MainRouterParamList>;
 export type SearchParams = StackNavigationProp<SearchParamList>;
 export type SettingsParams = StackNavigationProp<SettingsParamList>;
+
+/*** WelcomScreen ***/
+export type WelcomScreenType = 'makeSpace' | 'enterSpace' | 'beMember';
+export type WelcomScreenProps = RouteProp<InvitationParamList, 'Welcome'>;
