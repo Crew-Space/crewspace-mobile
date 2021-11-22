d56af922e4c99f7faa89f5ffb24a0d5c89f2569c
eunseonglee <eunseongofficial@gmail.com>
2021-11-23 04:38:56 +0900

    [#5] feat: 홈스크린의 상수값

---
+++ mobile/src/containers/Main/HomeScreen/constant.ts
@@ -0,0 +1,7 @@
+import { normalize } from 'utils';
+
+export const STICKY_EXPANDABLE_HEADER_HEIGHT = normalize(60);
+
+export const HEADER_MAX_HEIGHT = 136;
+export const HEADER_MIN_HEIGHT = 60;
+export const HEADER_SCROLL_DISTANCE = HEADER_MAX_HEIGHT - HEADER_MIN_HEIGHT;
