412ed7708003daaf1154d1f7b87d388fb92c1c73
eunseonglee <eunseongofficial@gmail.com>
2021-11-23 04:41:16 +0900

    [#5] feat: 홈스크린 스크롤 애니메이션 추가

--- mobile/src/containers/Main/HomeScreen/index.tsx
+++ mobile/src/containers/Main/HomeScreen/index.tsx
@@ -1,87 +1,95 @@
-import React from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React, { useRef, useState } from 'react';
+import { Animated, RefreshControl, StyleSheet, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 
-import { MainRouterParams, RootRouterParams } from 'types/Route';
-import { BACKGROUND, GRAY2, LINE, WHITE } from 'theme/Colors';
-import Text from 'components/Text';
-import PinnedNotice from './PinnedNotice';
+import { RootRouterParams } from 'types/Route';
+import { WHITE } from 'theme/Colors';
 import PostPreview from 'components/PostPreview';
-import BottomTabSafeAreaView from 'components/BottomTabSafeAreaView';
+import PinnedNoticeList from './PinnedNoticeList';
+import SectionHeader from 'components/SectionHeader';
+import HomeHeader from './HomeHeader';
+import { HEADER_MAX_HEIGHT, STICKY_EXPANDABLE_HEADER_HEIGHT } from './constant';
 
 const HomeScreen = () => {
   const navigation = useNavigation<RootRouterParams>();
+  const scrollYState = useRef(new Animated.Value(0)).current;
+  const [refreshing, setRefreshing] = useState(false);
+  const ScrollViewProps = {
+    scrollEventThrottle: 16,
+    onScroll: Animated.event([{ nativeEvent: { contentOffset: { y: scrollYState } } }], {
+      useNativeDriver: true,
+    }),
+    refreshControl: (
+      <RefreshControl
+        refreshing={refreshing}
+        onRefresh={() => {
+          setRefreshing(true);
+          setTimeout(() => setRefreshing(false), 1000);
+        }}
+        progressViewOffset={HEADER_MAX_HEIGHT + STICKY_EXPANDABLE_HEADER_HEIGHT}
+      />
+    ),
+    contentInset: {
+      top: HEADER_MAX_HEIGHT + STICKY_EXPANDABLE_HEADER_HEIGHT,
+    },
+    contentOffset: {
+      x: 0,
+      y: -HEADER_MAX_HEIGHT,
+    },
+  };
+
   return (
-    <BottomTabSafeAreaView style={styles.container}>
-      <View>
-        <ScrollView>
-          <Text onPress={() => navigation.navigate('Search')}>Header</Text>
-          <FlatList
-            horizontal
-            contentContainerStyle={{
-              paddingVertical: 17,
-              paddingHorizontal: 15,
-              backgroundColor: BACKGROUND,
+    <View style={styles.container}>
+      <Animated.ScrollView {...ScrollViewProps}>
+        <PinnedNoticeList
+          data={[
+            {
+              subText: {
+                left: '일반 공지',
+                right: '2021.10.14(금)',
+              },
+              Title: '10월 3차 오프 모임 안내',
+            },
+          ]}
+        />
+        <SectionHeader
+          text={'최근 공지'}
+          subText={'더 보기'}
+          onSubTextPress={() =>
+            navigation.navigate('Main', {
+              screen: 'Notice',
+            })
+          }
+        />
+        {Array.from('-'.repeat(10)).map(() => (
+          <PostPreview
+            header={{
+              subText: { left: '과제 공지', right: '10분 전' },
+              Title: '1차 과제 마감 안내',
             }}
-            data={Array.from('-'.repeat(6))}
-            renderItem={() => (
-              <PinnedNotice
-                header={{
-                  subText: {
-                    left: '일반 공지',
-                    right: '2021.10.14(금)',
-                  },
-                  Title: '10월 3차 오프 모임 안내',
-                }}
-              />
-            )}
+            description={
+              '안녕하세요 :) 1차 과제 마감 관련하여 공지드립니다. 사전에 고지드린대로 인당 3개씩 아이디어 조사하여, 간단히 PPT 자료 제작해오시면 될 것 같습니다. 궁금한 점 언제든 문의...'
+            }
+            isSaved={false}
+            viewed={false}
           />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              paddingVertical: 19,
-              paddingHorizontal: 20,
-              borderBottomColor: LINE,
-              borderBottomWidth: 1,
-              backgroundColor: WHITE,
-            }}>
-            <Text fontType={'BOLD_18'}>최근 공지</Text>
-            <Text
-              fontType={'REGULAR_14'}
-              color={GRAY2}
-              onPress={() =>
-                navigation.navigate('Main', {
-                  screen: 'Notice',
-                })
-              }>
-              더 보기
-            </Text>
-          </View>
-          {Array.from('-'.repeat(10)).map(() => (
-            <PostPreview
-              header={{
-                subText: { left: '과제 공지', right: '10분 전' },
-                Title: '1차 과제 마감 안내',
-              }}
-              description={
-                '안녕하세요 :) 1차 과제 마감 관련하여 공지드립니다. 사전에 고지드린대로 인당 3개씩 아이디어 조사하여, 간단히 PPT 자료 제작해오시면 될 것 같습니다. 궁금한 점 언제든 문의...'
-              }
-              isSaved={false}
-              viewed={false}
-            />
-          ))}
-        </ScrollView>
-      </View>
-    </BottomTabSafeAreaView>
+        ))}
+      </Animated.ScrollView>
+
+      <HomeHeader
+        scrollYState={scrollYState}
+        headerImageUrl={
+          'https://opgg-com-image.akamaized.net/attach/images/20210308225137.861310.jpg'
+        }
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: BACKGROUND,
+    backgroundColor: WHITE,
   },
 });
 
