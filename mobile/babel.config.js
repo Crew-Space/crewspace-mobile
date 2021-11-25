2c24a45f909355d591fe821c2a1d244a063ef1d8
eunseonglee <eunseongofficial@gmail.com>
2021-11-26 03:57:31 +0900

    chore: dotenv 모듈 추가

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,6 +2,16 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+    [
       'module-resolver',
       {
         root: ['./src'],
