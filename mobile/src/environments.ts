32a7bbc6a72cf854bd38f56cf36cab19adfe8b21
eunseonglee <eunseongofficial@gmail.com>
2021-11-26 02:13:49 +0900

    fix: authUrl 수정

--- mobile/src/environments.ts
+++ mobile/src/environments.ts
@@ -1,9 +1,9 @@
 import { DEV_BASE_URL, PROD_BASE_URL, KAKAO_AUTH_REST_API_ID, DUMMY_TOKEN } from '@env';
 
 const ENV = {
-  apiUrl: __DEV__ ? DEV_BASE_URL : PROD_BASE_URL,
+  apiUrl: `${__DEV__ ? DEV_BASE_URL : PROD_BASE_URL}/v1`,
+  authUrl: `${__DEV__ ? DEV_BASE_URL : PROD_BASE_URL}/auth`,
   kakaoClientId: KAKAO_AUTH_REST_API_ID,
-  redirectUrl: `${__DEV__ ? DEV_BASE_URL : PROD_BASE_URL}/auth`,
   token: DUMMY_TOKEN,
 };
 
