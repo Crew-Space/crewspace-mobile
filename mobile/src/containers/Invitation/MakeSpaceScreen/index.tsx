96418b40e7dc00af45c1ac0cd6b58302565bedee
eunseonglee <eunseongofficial@gmail.com>
2021-12-01 04:55:06 +0900

    type: 오타 수정

--- mobile/src/containers/Invitation/MakeSpaceScreen/index.tsx
+++ mobile/src/containers/Invitation/MakeSpaceScreen/index.tsx
@@ -18,7 +18,6 @@ import CrewOnError from 'components/CrewOnError';
 import Step1 from './Step1';
 import Step2 from './Step2';
 import Step3 from './Step3';
-import MakeSpaceScreen from '../EnterSpaceScreen';
 
 type StepType = 1 | 2 | 3;
 
