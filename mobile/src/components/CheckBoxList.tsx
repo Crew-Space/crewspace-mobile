f80c8c687d3d8ca1cae0f0b8cf9db092aeec04b6
eunseonglee <eunseongofficial@gmail.com>
2021-12-03 06:25:37 +0900

    feat: CheckBoxList와 List 구현

---
+++ mobile/src/components/CheckBoxList.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { BLACK, GRAY2, LINE, WHITE } from 'theme/Colors';
+import CheckBox from './CheckBox';
+import Text from './Text';
+
+interface Props {
+  title?: string;
+  data: string[];
+  value: { key: string; value: boolean }[];
+  onValueChange: (value: { key: string; value: boolean }[]) => void;
+  fill?: string;
+  width?: number;
+}
+
+const CheckBoxList = ({ title, data, value, onValueChange, fill = BLACK, width = 24 }: Props) => {
+  return (
+    <View style={styles.container}>
+      {!!title && (
+        <View style={styles.item}>
+          <Text fontType={'BOLD_16'}>{title}</Text>
+        </View>
+      )}
+      {data.map((name, index) => (
+        <View key={index} style={[styles.item, styles.checkList]}>
+          <CheckBox
+            isChecked={value[index].value}
+            width={width}
+            fill={fill}
+            onPress={() => {
+              onValueChange([
+                ...value.slice(0, index),
+                { ...value[index], value: !value[index].value },
+                ...value.slice(index + 1),
+              ]);
+            }}
+          />
+          <Text key={index} color={value[index].value ? fill : GRAY2} style={styles.checkText}>
+            {name}
+          </Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: WHITE,
+  },
+  item: {
+    paddingVertical: 18,
+    paddingHorizontal: 20,
+    borderBottomColor: LINE,
+    borderBottomWidth: 1,
+  },
+  checkList: {
+    flexDirection: 'row',
+  },
+  checkText: {
+    marginLeft: 8,
+  },
+});
+export default CheckBoxList;
---
+++ mobile/src/components/ReadonlyList.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { LINE, WHITE } from 'theme/Colors';
+import Text from './Text';
+
+interface Props {
+  title?: string;
+  data: string[];
+}
+
+const ReadonlyList = ({ title, data }: Props) => {
+  return (
+    <View style={styles.container}>
+      {!!title && (
+        <View style={styles.item}>
+          <Text fontType={'BOLD_16'}>{title}</Text>
+        </View>
+      )}
+      {data.map((name, index) => (
+        <View style={styles.item}>
+          <Text key={index}>{name}</Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: WHITE,
+  },
+  item: {
+    paddingVertical: 18,
+    paddingHorizontal: 20,
+    borderBottomColor: LINE,
+    borderBottomWidth: 1,
+  },
+});
+export default ReadonlyList;
