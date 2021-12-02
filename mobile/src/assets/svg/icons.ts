16b02723aff0b2a7f929bb7afd770a7e9067e79b
eunseonglee <eunseongofficial@gmail.com>
2021-12-03 06:25:21 +0900

    feat: CheckBox 컴포넌트 구현

--- mobile/src/assets/svg/icons.ts
+++ mobile/src/assets/svg/icons.ts
@@ -113,6 +113,16 @@ export const pin = {
     </svg>
     `,
 };
+export const checkBox = {
+  on: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M19 3H5C3.89 3 3 3.9 3 5V19C3 20.1 3.89 21 5 21H19C20.11 21 21 20.1 21 19V5C21 3.9 20.11 3 19 3ZM10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" />
+  </svg>
+  `,
+  off: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M19 5V19H5V5H19ZM19 3H5C3.9 3 3 3.9 3 5V19C3 20.1 3.9 21 5 21H19C20.1 21 21 20.1 21 19V5C21 3.9 20.1 3 19 3Z" />
+  </svg>
+  `,
+};
 
 export const save = {
   on: `<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
---
+++ mobile/src/components/CheckBox.tsx
@@ -0,0 +1,24 @@
+import { checkBox } from 'assets/svg/icons';
+import React from 'react';
+import { BLACK, GRAY2 } from 'theme/Colors';
+import SvgIcon from './SvgIcon';
+
+interface Props {
+  isChecked: boolean;
+  width?: number;
+  fill?: string;
+  onPress: () => void;
+}
+
+const CheckBox = ({ isChecked, width = 24, fill = BLACK, onPress }: Props) => {
+  return (
+    <SvgIcon
+      onPress={onPress}
+      xml={isChecked ? checkBox.on : checkBox.off}
+      fill={isChecked ? fill : GRAY2}
+      width={width}
+    />
+  );
+};
+
+export default CheckBox;
