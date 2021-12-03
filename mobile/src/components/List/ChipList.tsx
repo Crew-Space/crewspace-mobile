3b0c2ca4fa29ca0079b919ebf797483a7d1e8524
eunseonglee <eunseongofficial@gmail.com>
2021-12-04 03:30:58 +0900

    feat: ChipList 컴포넌트 구현

---
+++ mobile/src/components/List/ChipList.tsx
@@ -0,0 +1,60 @@
+import Text from 'components/Text';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { GRAY1, GRAY3, PRIMARY } from 'theme/Colors';
+
+interface Props {
+  defaultValue?: boolean[];
+  data: string[];
+  onValueChange: (value: boolean[]) => void;
+  onlyOneSelected?: boolean;
+}
+
+const ChipList = ({ defaultValue, data, onValueChange, onlyOneSelected = false }: Props) => {
+  const [selected, setSelected] = useState(defaultValue || data.map((_) => false));
+
+  const onTouchEnd = (index: number) => {
+    const newState = (onlyOneSelected ? data.map((_) => false) : selected).map((d, idx) =>
+      index === idx ? !d : d,
+    );
+    setSelected(newState);
+    onValueChange(newState);
+  };
+
+  return (
+    <View style={styles.list}>
+      {data.map((chip, index) => (
+        <View
+          key={index}
+          style={[
+            styles.categoryItem,
+            {
+              borderColor: selected[index] ? PRIMARY : GRAY3,
+            },
+          ]}
+          onTouchEnd={() => onTouchEnd(index)}>
+          <Text fontType={'REGULAR_14'} color={selected[index] ? PRIMARY : GRAY1}>
+            {chip}
+          </Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  list: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  categoryItem: {
+    alignSelf: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 18,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginRight: 10,
+    marginBottom: 10,
+  },
+});
+export default ChipList;
