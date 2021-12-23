c21d0f06af39ab052785c9c9f825d3b5937d9d57
eunseonglee <eunseongofficial@gmail.com>
2021-12-23 23:10:34 +0900

    chore: 필요한 설정 추가

--- crewspace-web/next.config.js
+++ crewspace-web/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    if (process.env.NODE_ENV !== 'production') {
+      return [
+        {
+          destination: process.env.DESTINATION_URL,
+          source: process.env.SOURCE_PATH,
+        },
+      ];
+    }
+  },
+};
